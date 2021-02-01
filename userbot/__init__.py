de309eb369749bbefc09a958d75c3744c30406d5
winterfell29 <78132487+winterfell29@users.noreply.github.com>
2021-02-01 05:36:00 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -59,7 +59,7 @@ if bool(ENV):
     # Check if the config was edited by using the already used variable.
     # Basically, its the 'virginity check' for the config file ;)
     CONFIG_CHECK = os.environ.get(
-        "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+        "", None)
 
     if CONFIG_CHECK:
         LOGS.info(
